cb47290aa48dac222b03b3015a6583ec8e7de0cb
MPOfficial <milkproducktionskirby@gmail.com>
2016-01-27 17:46:35 -0500

    Update

--- src/dream/core/Main.java
+++ src/dream/core/Main.java
@@ -74,7 +74,7 @@ public class Main {
 	}
 	
 	public void gameloop() {
-		final double GAME_HERTZ = 30.0;
+		final double GAME_HERTZ = 60.0;
 		final double TIME_BETWEEN_UPDATES = 1000000000 / GAME_HERTZ;
 		final int MAX_UPDATES_BEFORE_RENDER = 5;
 		double lastUpdateTime = System.nanoTime();
